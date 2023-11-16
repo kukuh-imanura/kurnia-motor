6d6a682b1378144075f27ace7125b618cd698be7
kukuh-imanura <kukuh.imanura.bagaskara@gmail.com>
2023-11-16 09:23:07 +0800

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import {Button} from '@/components/ui/button'
+import Icon from '../public/assets/icons/loader.svg'
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen">
+      <img className='m-3' src={Icon} alt="Add Post Icon" />
       <Button className="shad-button_primary">Click me</Button>
     </div>
   )
