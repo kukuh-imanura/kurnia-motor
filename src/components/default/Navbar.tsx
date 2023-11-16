bcaf47fb3b804d8a96e45975575968d4715eddc6
kukuh-imanura <kukuh.imanura.bagaskara@gmail.com>
2023-11-16 18:34:05 +0800

    update Navbar.tsx

--- src/components/default/Navbar.tsx
+++ src/components/default/Navbar.tsx
@@ -1,8 +1,18 @@
-
+import logo from '../../../public/assets/images/logo-orange.png'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="text-black py-3 px-10 flex justify-between items-center" style={{backgroundColor : '#FFEDD3'}}>
+      <img src={logo} alt="" className='w-44'/>
+
+      <div className='space-x-10'>
+        <a href="" className='font-bold' style={{color : '#F18805'}}>Beranda</a>
+        <a href="" className=''>Layanan</a>
+        <a href="" className=''>Tentang Kami</a>
+      </div>
+
+      <button className='px-10 h-11 text-white rounded font-bold' style={{backgroundColor : '#F18805'}}>Keluar</button>
+    </div>
   )
 }
 
