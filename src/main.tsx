aada1901b61c150819b6387fe93d54993c606114
yaqinzz <ahmadainulyaqin131@gmail.com>
2023-11-22 08:24:14 +0700

    admin

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './globals.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home.tsx'
-import NotFound from './pages/NotFound.tsx'
-import About from './pages/About.tsx'
-import Customer from './pages/Customer.tsx'
-import Admin from './pages/Admin.tsx'
-import Laporan from './pages/Laporan.tsx'
-import Detail from './pages/Detail.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./globals.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import NotFound from "./pages/NotFound.tsx";
+import About from "./pages/About.tsx";
+import Customer from "./pages/Customer.tsx";
+import Admin from "./pages/Admin.tsx";
+import Laporan from "./pages/Laporan.tsx";
+import Detail from "./pages/Detail.tsx";
+import AddUserAdmin from "./components/default/AddUserAdmin.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
@@ -20,8 +21,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path="/admin" element={<Admin />} />
         <Route path="/laporan" element={<Laporan />} />
         <Route path="/about" element={<About />} />
+        <Route path="/add" element={<AddUserAdmin />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
