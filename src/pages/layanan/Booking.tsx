4f4ef577a28452c57ecc40df048395fdf16b7ef1
kukuh-imanura <kukuh.imanura.bagaskara@gmail.com>
2023-12-19 12:26:48 +0800

    update booking

--- src/pages/layanan/Booking.tsx
+++ src/pages/layanan/Booking.tsx
@@ -1,26 +1,54 @@
 import Navbar from "@/components/default/Navbar";
 import Pagination from "@/components/default/Pagination";
 import Sidebar from "@/components/default/Sidebar";
-import Table from "@/components/default/Table";
 import Button from "@/components/default/Button";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 function Booking() {
   const [dataBooking, setDataBooking] = useState<any[]>([])
+  const row = 5
+  const [thisPage, setThisPage] = useState(1)
 
   useEffect(() => {
     axios
       .get(`https://bengkel-api-ruby.vercel.app/api/booking`)
       .then((response) => {
-        setDataBooking(response.data.result);
+        setDataBooking(response.data.result)
+        console.log(response.data.result)
       })
       .catch((error) => {
         return "Error : " + error;
       });
   }, [])
 
-  console.log(dataBooking)
+  // FILTER DATA PENDING
+  const dataPending = dataBooking?.filter((value:any) => value.status === "Pending" || value.status === "pending")
+  const totalPage = Math.ceil(dataPending.length/row)
+
+  // MEMBUAT ID (PENOMORAN)
+  const numDataBooking = dataPending?.map((value:any, index:any) => {
+    return{
+      no: index + 1,
+      ...value
+    }
+  })
+
+  // MEMBAGI HALAMAN
+  const getDataForPage = (pageNumber:any) => {
+    const startIndex = (pageNumber - 1) * row;
+    const endIndex = startIndex + row;
+    return numDataBooking.slice(startIndex, endIndex);
+  };
+
+  // HANDLE TERIMA/TOLAK
+  const handleTerima = (id:any) => {
+    alert(`Terima ${id}`)
+  }
+  const handleTolak = (id:any) => {
+    alert(`Tolak ${id}`)
+  }
+
   return (
     <div className="h-screen bg-surface-1 text-gray-900 flex">
       <Sidebar />
@@ -33,18 +61,16 @@ function Booking() {
             <p className="text-center text-xl text-brand-1">Pastikan Customer menerima pelayanan terbaik anda!</p>
             <p className="text-center text-xl text-brand-1 mb-5">Booking online untuk memperkirakan kinerja yang kamu berikan</p>
 
-            {/* <Table.Default className="h-full"/> */}
-
             <div>
               <table className="w-full">
                 <tbody>
 
                   {
-                    dataBooking?.map((value:any, index:any) => {
+                    getDataForPage(thisPage)?.map((value:any, index:any) => {
                       return(
                         <tr key={index} className={`flex items-center justify-between p-3 ${index % 2 === 0 ? "" : "border-y-2"}`}>
                           <td className="flex items-center gap-5">
-                            <p>{index + 1}.</p>
+                            <p>{value.no}.</p>
                             <div>
                               <img src={value.pict || "/assets/images/admin/default.jpg"} alt="Foto Profil" className="h-16 rounded-full" />
                             </div>
@@ -57,8 +83,8 @@ function Booking() {
                           <td>
                             <div className="flex gap-3">
                               {/* Kalau di terima/Tolek, ambil ID dari dataBooking, dan kemudian ubah status dari data tersebut menjadi (diproses atau ditolak) */}
-                              <Button.Default text="Terima" />
-                              <Button.Default text="Tolak" />
+                              <Button.Default text="Terima" onClick={() => handleTerima(value.id_booking)} />
+                              <Button.Default text="Tolak" onClick={() => handleTolak(value.id_booking)} />
                             </div>
                           </td>
                         </tr>
@@ -70,7 +96,8 @@ function Booking() {
               </table>
             </div>
           </div>
-          <Pagination />
+
+          <Pagination totalPage={totalPage} halamanIni={(data:any) => setThisPage(data + 1)}/>
         </div>
       </div>
     </div>
